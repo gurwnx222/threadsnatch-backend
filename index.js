bfa2437834cb87fc6c6ae93643511c757069e3dd
Gurwinder Singh <threader.corp@gmail.com>
2025-05-06 01:49:25 +0000

    New commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import express from 'express';
+import express from "express";
+import { connectToDatabase } from "./dbConfig.js";
+import router from "./routes/emailDb.js";
 
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  connectToDatabase();
 });
+app.use("/api", router);
 
-app.listen(3000, () => {
-  console.log('Express server initialized');
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
